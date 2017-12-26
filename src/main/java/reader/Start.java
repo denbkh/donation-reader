99be5a4475f007fdd6472657ad0bd9d604b7055b
Denis Bakharev <den.bkh@gmail.com>
2017-12-26 21:57:08 +0300

    set pippo to 1.5.0

--- src/main/java/reader/Start.java
+++ src/main/java/reader/Start.java
@@ -1,5 +1,8 @@
 package reader;
 
+import javax.servlet.http.Cookie;
+
+import com.google.appengine.api.datastore.*;
 import ro.pippo.core.Application;
 import ro.pippo.freemarker.FreemarkerTemplateEngine;
 
@@ -11,9 +14,26 @@ public class Start extends Application {
     @Override
     protected void onInit() {
 
+        DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+
         GET("/", routeContext -> {
-            routeContext.setLocal("greeting", "Тест New1234");
-            routeContext.render("hello");
+
+//            Entity employee = new Entity("Employee", "asalieri");
+//            employee.setProperty("firstName", "Antonio1");
+//            employee.setProperty("lastName", "Salieri1");
+//
+//            datastore.put(employee);
+
+            Cookie refreshToken = routeContext.getRequest().getCookie("refresh_token");
+
+            try {
+                Entity entity = datastore.get(KeyFactory.createKey("Employee", "asalieri"));
+                routeContext.setLocal("greeting", entity.getProperty("lastName"));
+            } catch (EntityNotFoundException e) {
+                routeContext.setLocal("greeting", "not found");
+            }
+
+            routeContext.render("index");
         });
 
         addPublicResourceRoute();
